a3c81133400fb8712769fdc636c473f75e85defd
ackintosh <sora.akatsuki@gmail.com>
2020-08-09 09:54:37 +0900

    mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,44 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// 合計値を格納する sum を初期化
+@sum
+M=0
+// ループ用のカウンタ i を初期化
+@i
+M=0
+
+(LOOP)
+	// ループが目的の回数に達したらENDにジャンプする
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JEQ
+
+	// 加算
+	@R0
+	D=M
+	@sum
+	M=D+M
+
+	// カウンタをインクリメント
+	@i
+	M=M+1
+	// ループの先頭に戻る
+	@LOOP
+	0;JMP
+
+@END
+D;JEQ
+(END)
+	// 結果sumをRAM(R2)に保存する
+	@sum
+	D=M // D=sum
+	@R2
+	M=D // R2=sum
+
+	// 終了
+	@END
+	0;JMP
