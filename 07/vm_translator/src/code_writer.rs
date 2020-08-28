9dd1b4d4b24d7d9206be11508c7a933a2730be78
ackintosh <sora.akatsuki@gmail.com>
2020-08-28 19:08:54 +0900

    SimpleAdd

--- 07/vm_translator/src/code_writer.rs
+++ 07/vm_translator/src/code_writer.rs
@@ -1,16 +1,99 @@
 use crate::parser::Command;
+use crate::parser::MemorySegment;
+use crate::parser::Operator;
 
 // 表7-2 CodeWriterモジュール
 pub struct CodeWriter {
+    sp: StackPointer,
+}
+
+struct StackPointer {
+    address: u16,
+}
+
+impl StackPointer {
+    pub fn new() -> Self {
+        Self {
+            address: 256,
+        }
+    }
+
+    pub fn increment(&mut self) -> u16 {
+        let a = self.address;
+        self.address += 1;
+        if self.address > 2047 {
+            panic!("Out of stack memory space")
+        }
+
+        return a;
+    }
+
+    pub fn decrement(&mut self) -> u16 {
+        self.address -= 1;
+        if self.address < 256 {
+            panic!("Out of stack memory space")
+        }
+
+        return self.address;
+    }
+
+    pub fn current(&self) -> u16 {
+        self.address
+    }
 }
 
 impl CodeWriter {
-    pub fn code(&self, command: Command) -> String {
+    pub fn new() -> Self {
+        Self {
+            sp: StackPointer::new(),
+        }
+    }
+
+    pub fn code(&mut self, command: Command) -> Vec<String> {
         match command {
             Command::Push(segment, index) => {
-                todo!()
+                match segment {
+                    MemorySegment::Constant => {
+                        let mut a = vec![
+                            format!("@{}", index),
+                            "D=A".into(),
+                            format!("@{}", self.sp.increment()),
+                            "M=D".into(),
+                        ];
+                        a.append(&mut self.set_sp());
+                        a
+                    }
+                    _ => todo!()
+                }
+            }
+            Command::Arithmetic(operator) => {
+                match operator {
+                    Operator::Add => {
+                        let mut a = vec![
+                            format!("@{}", self.sp.decrement()),
+                            "D=M".into(),
+                            format!("@{}", self.sp.decrement()),
+                            "D=D+M".into(),
+                            format!("@{}", self.sp.increment()),
+                            "M=D".into(),
+                        ];
+                        a.append(&mut self.set_sp());
+                        a
+                    }
+                    _ => todo!(),
+                }
             }
             _ => todo!()
         }
+    }
+
+    // スタックポインタを更新するアセンブリコードを返す
+    fn set_sp(&self) -> Vec<String> {
+        vec![
+            format!("@{}", self.sp.current()),
+            "D=A".into(),
+            "@SP".into(),
+            "M=D".into(),
+        ]
     }
 }
--- 07/vm_translator/src/main.rs
+++ 07/vm_translator/src/main.rs
@@ -1,5 +1,8 @@
 use crate::parser::Parser;
 use crate::code_writer::CodeWriter;
+use std::path::PathBuf;
+use std::io::{BufWriter, Write};
+use std::fs::File;
 
 mod code_writer;
 mod parser;
@@ -19,17 +22,27 @@ fn main() {
         std::fs::File::open(path).expect("failed to open the file")
     );
 
-    let mut assembly_commands = vec![];
-    let mut code_writer = CodeWriter{};
+    let mut assembly_codes = vec![];
+    let mut code_writer = CodeWriter::new();
 
     loop {
         if let Some(command) = parser.advance() {
             println!("command: {:?}", command);
-            assembly_commands.push(code_writer.code(command));
+            assembly_codes.append(&mut code_writer.code(command));
         } else {
             break;
         }
     }
 
-    println!("assembly_commands: {:?}", assembly_commands);
+    println!("assembly_codes: {:?}", assembly_codes);
+
+    let mut output_path = PathBuf::from(path);
+    output_path.set_extension("asm");
+
+    let mut writer = BufWriter::new(File::create(output_path).expect("failed to create asm file"));
+    for code in assembly_codes.iter() {
+        writer.write_all(code.as_bytes()).expect("failed to write assembly codes");
+        writer.write_all(b"\n").unwrap();
+    }
+
 }
